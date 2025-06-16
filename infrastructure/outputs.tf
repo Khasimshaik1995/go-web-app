e85effa1e5e1a043fa8c6d575e353616dad3ef92
Your Name <you@example.com>
2025-06-16 15:15:57 +0600

    backup

---
+++ infrastructure/outputs.tf
@@ -0,0 +1,3 @@
+output "kube_config_command" {
+  value = "az aks get-credentials --resource-group ${azurerm_resource_group.main.name} --name ${azurerm_kubernetes_cluster.main.name}"
+}
--- infrastructure/vnet/main.tf
+++
@@ -1,7 +0,0 @@
-resource "azurerm_virtual_network" "main" {
-  name = "vnet-1"
-  location = var.location
-  resource_group_name = var.rg_name
-  address_space = var.address_space
-  tags = var.tags
-}
