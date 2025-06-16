c931a4670cbbb6f8f910fdf648869985ac54b186
Your Name <you@example.com>
2025-06-17 05:36:25 +0600

    backup

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -1,8 +1,3 @@
-variable "subscription" {
-  description = "The subscription id of azure"
-  type        = string
-}
-
 variable "location" {
   description = "Location of all resources"
   type        = string
